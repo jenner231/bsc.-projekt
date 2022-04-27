e1a7e04d0f4db67babde53684803b93ffdb215b9
jenner231 <jenner231@users.noreply.github.com>
2022-04-27 16:30:16 +0200

    Update sx126x.py
    test

--- python/sx126x.py
+++ python/sx126x.py
@@ -262,14 +262,14 @@ class sx126x:
             r_buff = self.ser.read(self.ser.inWaiting())
             #####Made a check to see if the message was for us
             ##### TODO: Make the else statement reroute the message to the right owner if in routing table or send to next hop closer to the right owner if not directly connected.
-            if r_buff[2] == self.node_id:
-                print("receive message from node address with frequence\033[1;32m %d,%d,%d.125MHz\033[0m"%((r_buff[0]<<8)+r_buff[1],r_buff[3]+self.start_freq),end='\r\n',flush = True)
+            if r_buff[4] == self.node_id:
+                print("receive message from node address with frequence\033[1;32m %d,%d,%d.125MHz\033[0m"%((r_buff[0]<<8)+r_buff[1],r_buff[2]+self.start_freq),end='\r\n',flush = True)
                 print("message is "+str(r_buff[4:-1]),end='\r\n')
             else:
-                print("This message was not meant for us, it was meant for %d"%r_buff[2])
+                print("This message was not meant for us, it was meant for %d"%r_buff[4])
                 for i in r_buff:
                     print(r_buff[i])
-                    
+
             
             # print the rssi
             if self.rssi:
