db7ed55e92e76ccbc9b3c871ecd5454455d94354
jenner231 <jenner231@users.noreply.github.com>
2022-05-13 11:05:54 +0200

    Update main_test.py

--- python/main_test.py
+++ python/main_test.py
@@ -184,7 +184,7 @@ async def async_main():
                         break
 
             sys.stdout.flush()
-
+        print("test")
         node.receive()
 
         # timer,send messages automatically
