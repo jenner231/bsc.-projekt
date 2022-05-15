b3d28bad9aae0b73cf5596ea1a398edebf607312
jenner231 <jenner231@users.noreply.github.com>
2022-05-15 11:17:37 +0200

    Update main_test.py

--- python/main_test.py
+++ python/main_test.py
@@ -157,13 +157,14 @@ async def cancel_cpu(cont):
     ##### But if we do press c, we want to cancel both
     while cancel_cont:
         print(time)
-        if sys.stdin.read(1) == '\x63':
-            print('\x1b[1A', end='\r')
-            print(" " * 100)
-            print('\x1b[1A', end='\r')
-            cont = False
-            cancel_cont = cont
-            return cont
+        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
+            if sys.stdin.read(1) == '\x63':
+                print('\x1b[1A', end='\r')
+                print(" " * 100)
+                print('\x1b[1A', end='\r')
+                cont = False
+                cancel_cont = cont
+                return cont
         if 5 < time:
             cancel_cont = False
             return cont
