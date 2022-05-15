eae320409cbe568d24c9e4aa173325631685adea
jenner231 <jenner231@users.noreply.github.com>
2022-05-15 14:42:07 +0200

    123

--- python/sx126x.py
+++ python/sx126x.py
@@ -92,7 +92,7 @@ class sx126x:
         32:SX126X_PACKAGE_SIZE_32_BYTE
     }
 
-    def __init__(self,serial_num,freq,addr,ack_info,power,rssi,air_speed=2400,\
+    def __init__(self,serial_num,freq,addr,power,rssi,air_speed=2400,\
                  net_id=0,buffer_size = 240,crypt=0,\
                  relay=False,lbt=False,wor=False):
         self.rssi = rssi
@@ -291,6 +291,7 @@ class sx126x:
     def receive_ack(self, r_buff):
         #####Store tuple of (ack_id, sender address) 
             self.ack_info = (r_buff[3], ((r_buff[0]<<8) + r_buff[1]))
+            print(self.ack_info)
 
 
     def get_ack(self):
