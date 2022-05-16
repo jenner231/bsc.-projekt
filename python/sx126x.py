539a2c7a15fd52954de896e868a43bc4794cd8c5
jenner231 <jenner231@users.noreply.github.com>
2022-05-16 14:56:45 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -354,10 +354,6 @@ class sx126x:
             else:
                 pass
                 #print('\x1b[2A',end='\r')
-            if self.ser.inWaiting() > 0:
-                self.receive()
-            else:
-                pass
 
     def get_channel_rssi(self):
         GPIO.output(self.M1,GPIO.LOW)
@@ -367,9 +363,9 @@ class sx126x:
         self.ser.write(bytes([0xC0,0xC1,0xC2,0xC3,0x00,0x02]))
         time.sleep(0.1)
         re_temp = bytes(5)
-        #if self.ser.inWaiting() > 0:
-         #   time.sleep(0.1)
-          #  re_temp = self.ser.read(self.ser.inWaiting())
+        if self.ser.inWaiting() > 0:
+            time.sleep(0.1)
+            re_temp = self.ser.read(self.ser.inWaiting())
         if re_temp[0] == 0xC1 and re_temp[1] == 0x00 and re_temp[2] == 0x02:
             print("the current noise rssi value: -{0}dBm".format(256-re_temp[3]))
             # print("the last receive packet rssi value: -{0}dBm".format(256-re_temp[4]))
