0c05ec296db647026d320d978f5d00faef7a4235
jenner231 <jenner231@users.noreply.github.com>
2022-05-20 09:36:27 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -325,6 +325,7 @@ class sx126x:
 
 
     def check_message(self, r_buff):
+        print("check message checkpoint 1")
         visited = False
         #####Check we have visited this not before to avoid infinite loop when flooding the network in broadcasts
         for i in r_buff[5]:
@@ -380,7 +381,9 @@ class sx126x:
             time.sleep(0.05)
             print("receive checkpoint 1")
             r_buff = self.ser.read(self.ser.inWaiting())
-            print(int(chr(r_buff[3])))
+            print(int(chr(r_buff[4])))
+            print(r_buff[5])
+            print(r_buff[6])
             #####Made a check to see if the message was for us
             #r_buff[0] == receiving node address, r_buff[1] == sender node address, r_buff[2] == frequency, r_buff[3] == node_id of receiver, r_buff[4] == sender node_id, r_buff[5] == ack_id, r_buff[6]+ == payload
             ##### TODO: Make the else statement reroute the message to the right owner if in routing table or send to next hop closer to the right owner if not directly connected.
@@ -394,7 +397,7 @@ class sx126x:
                     self.reachable_dev.append((r_buff[0]<<8) + r_buff[1])
                     print(print("Node IDs in range: "+str(self.reachable_dev)))
             elif int(chr(r_buff[3])) == 1:
-                print("Receive message 2")
+                print("Receive checkpoint 2")
                 self.check_message(r_buff)
                 #self.receive_ack(r_buff)
                 #print("Noted ack_id")
