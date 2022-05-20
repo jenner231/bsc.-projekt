300c5a69c906800c8de24e3e90b90acdcbe4bae5
jenner231 <jenner231@users.noreply.github.com>
2022-05-20 09:31:33 +0200

    Update sx126x.py
    Today is all debugging

--- python/sx126x.py
+++ python/sx126x.py
@@ -378,6 +378,7 @@ class sx126x:
         if self.ser.inWaiting() > 0:
             #####TODO: Make sleep appropriate. If too small, it will not read the entire message!!
             time.sleep(0.05)
+            print("receive checkpoint 1")
             r_buff = self.ser.read(self.ser.inWaiting())
             #####Made a check to see if the message was for us
             #r_buff[0] == receiving node address, r_buff[1] == sender node address, r_buff[2] == frequency, r_buff[3] == node_id of receiver, r_buff[4] == sender node_id, r_buff[5] == ack_id, r_buff[6]+ == payload
