38b65dab7e9e6e206ffc715328b17c121bed01d8
jenner231 <jenner231@users.noreply.github.com>
2022-05-20 10:02:39 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -381,7 +381,7 @@ class sx126x:
             time.sleep(0.05)
             print("receive checkpoint 1")
             r_buff = self.ser.read(self.ser.inWaiting())
-            rec = r_buff.decode()
+            rec = r_buff[0].decode()
             print(rec)
             print("checkpoint 2")
             get_t = rec.split(",")
