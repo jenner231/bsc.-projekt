e8d9aaca20c12a0ea20cb28fd49e7ced835ee2f0
jenner231 <jenner231@users.noreply.github.com>
2022-05-21 10:49:32 +0200

    Update main_test.py

--- python/main_test.py
+++ python/main_test.py
@@ -121,8 +121,8 @@ async def request_cpu_continue():
     path = [node.addr]
     ack_id = 1
     print("check2")
-    time = datetime.time()
-    print(time)
+    time = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
+    print("checkpoint 3")
         #await asyncio.sleep(10)
         #global timer_task
         #global seconds
