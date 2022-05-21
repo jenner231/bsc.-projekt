23d81e2e426f02feb7d0a8157dc52abfb07f8461
jenner231 <jenner231@users.noreply.github.com>
2022-05-21 13:32:09 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -342,11 +342,9 @@ class sx126x:
         path = r_buff[4]
         ####must remove \\x to convert from string to int 
         sender = r_buff[1].split("\\x") 
-        print(type(self.addr))
-        print(type(int(path[0])))
-        print(path[0])
         for i in path:
-            if self.addr == int(i):
+            val = int(i)
+            if self.addr == val:
                 visited = True
                 break
 
