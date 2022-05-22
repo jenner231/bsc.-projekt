1243cd5d283d938e6b0d968ab417b9a83d439e98
jenner231 <jenner231@users.noreply.github.com>
2022-05-22 14:54:14 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -469,12 +469,8 @@ class sx126x:
                 s = int(dateT.strftime("%S"))
                 total_seconds = m + s
                 print("heartbeat check 2")
-                if self.reachable_dev:
-                    self.reachable_dev.append((int((r_buff[1]<<8) + r_buff[2]), total_seconds))
-                    print(self.reachable_dev)
-                else:
-                    self.reachable_dev[0] = (int((r_buff[1]<<8) + r_buff[2]), total_seconds)
-                    print(self.reachable_dev)
+                self.reachable_dev.append((int((r_buff[1]<<8) + r_buff[2]), total_seconds))
+
 
                 #self.reachable_dev[1] = self.reachable_dev[1] + str()
                 #self.reachable_dev[0] = self.reachable_dev[0] + str((r_buff[1]<<8) + r_buff[2])
