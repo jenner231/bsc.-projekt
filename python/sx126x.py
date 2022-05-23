0b103a508f066ac1140d972a57d5f9c1be50544f
jenner231 <jenner231@users.noreply.github.com>
2022-05-23 14:48:29 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -374,7 +374,8 @@ class sx126x:
 
         else:
             print("test")
-            print("Node: "+self.end_node + " "+payload)
+            print(self.end_node)
+            print("Node: "+self.end_node + " " + payload)
             self.end_node = ""
             self.ack_info = (r_buff[2], r_buff[5])
             self.send_ack = True
