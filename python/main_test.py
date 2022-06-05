fdaafbdd785c331fbc1d1f6fb7dd160c89d151f1
jenner231 <jenner231@users.noreply.github.com>
2022-06-05 14:52:22 +0200

    Update main_test.py

--- python/main_test.py
+++ python/main_test.py
@@ -95,13 +95,14 @@ async def heartbeat():
     #data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes([12]) + "CPU Temperature:".encode()+str(get_cpu_temp()).encode()+" C".encode()
     data = bytes([int(65535)>>8]) + bytes([int(65535)&0xff]) + bytes([offset_frequence]) + str(seperate).encode() + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + str(seperate).encode() + str(ack_id).encode() + str(seperate).encode() + str(time).encode() + str(seperate).encode()
     await asyncio.sleep(0.2)
+    print(data)
     node.send(data)
     print("We sent our heartbeat out")
     #await asyncio.sleep(1)
 
 #
 async def request_cpu_data():
-    print("req check 1")
+    #print("req check 1")
     #####Start out checking if we have nodes that we haven't heard from in a while
     node.compare_time()
     ###choice chooses a random i in the range 1-max number of nodes but excludes its own address
@@ -112,9 +113,9 @@ async def request_cpu_data():
 
     path = node.addr
     ack_id = 1
-    print("check2")
+    #print("check2")
     time = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
-    print("checkpoint 3")
+    #print("checkpoint 3")
         #await asyncio.sleep(10)
         #global timer_task
         #global seconds
@@ -169,12 +170,12 @@ async def send_ack():
         else:
             print(path)
             send_to = int(path[1])
-            print("Send ack check xxxxx")
+            #print("Send ack check xxxxx")
             path = path[1:]
             end_node = ack_inf[1]
             data = bytes([int(send_to)>>8]) + bytes([int(send_to)&0xff]) + bytes([offset_frequence]) + str(seperate).encode() + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + str(seperate).encode() + str(ack_id).encode() + str(seperate).encode() + str(path).encode() + str(seperate).encode() + str(end_node).encode() + str(seperate).encode()
 
-        print("Send ack check 2")
+        #print("Send ack check 2")
 
         #time = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
         # the sending message format
@@ -222,36 +223,37 @@ async def forward_ack():
         end_node = node.ack_info[1]
         #rand = float((random.randrange(0, 50, 3)) / 10)
         #await asyncio.sleep(rand)
-        print("forward_ack check 2")
+        #print("forward_ack check 2")
         offset_frequence = 18
         ack_id = 3
         path = node.ack_info[0]
         print(node.ack_info)
         if len(path) == 1:
-            print("forward_ack check path length 1")
+            #print("forward_ack check path length 1")
             send_to = end_node
-            print("forward_ack check 2 path length 1")
-            print(send_to)
+            #print("forward_ack check 2 path length 1")
+            #print(send_to)
             #####ack_inf[1]here is end_node set in ret_data function in sx126x
             data = bytes([int(send_to) >> 8]) + bytes([int(send_to) & 0xff]) + bytes([offset_frequence]) + str(
                 seperate).encode() + bytes([node.addr >> 8]) + bytes([node.addr & 0xff]) + bytes(
                 [node.offset_freq]) + str(seperate).encode() + str(ack_id).encode() + str(seperate).encode()
 
         else:
-            print("forward_ack check path length more than 1")
-            print(path)
+            #print("forward_ack check path length more than 1")
+            #print(path)
             send_to = int(path[1])
-            print("forward_ack check path length more than 2")
+            #print("forward_ack check path length more than 2")
             path = path[1:]
-            print("forward_ack check path length more than 3")
+            #print("forward_ack check path length more than 3")
             data = bytes([int(send_to) >> 8]) + bytes([int(send_to) & 0xff]) + bytes([offset_frequence]) + str(
                 seperate).encode() + bytes([node.addr >> 8]) + bytes([node.addr & 0xff]) + bytes(
                 [node.offset_freq]) + str(seperate).encode() + str(ack_id).encode() + str(seperate).encode() + str(
                 path).encode() + str(seperate).encode() + str(end_node).encode() + str(seperate).encode()
-            print("forward_ack check path length more than 4")
-        print("forward_ack check 4")
+            #print("forward_ack check path length more than 4")
+        #print("forward_ack check 4")
         await asyncio.sleep(0.2)
         node.send(data)
+        print(data)
         print("We successfully forwarded the acknowledgement message to the next hop in the path.")
         node.forward_ack = False
 
@@ -291,6 +293,7 @@ async def sleep_func():
 async def for_mes():
     if(node.forward != 0):
         #await sleep_func()
+        print("for_mes 1")
         seperate = ","
         in_reach = False
         #####Just setting variables for readability. We set forward in our chechk_message function in sx126x
@@ -307,6 +310,7 @@ async def for_mes():
             data = bytes([255]) + bytes([255]) + bytes([18]) + str(seperate).encode() + bytes([255]) + bytes([255]) + bytes([18]) + str(seperate).encode() + str(ack_id).encode() + str(seperate).encode() + str(end_node).encode() + str(seperate).encode() + str(path).encode() + str(seperate).encode() + str(time).encode() + str(seperate).encode()
         await asyncio.sleep(0.2)
         node.send(data)
+        print(data)
         print("We were not the requested node, successfully forwarded the request to my neighbours(or if i had the node in my cache, only it)")
         node.forward = 0
     else:
@@ -317,11 +321,12 @@ async def resp_data():
     ####if we have something in our path array, basically says if len(node.path) not empty
     if node.path:
         #await sleep_func()
+        print("resp_data 1")
         seperate = ","
         send_to = int(node.path[-1])
         #####back_path is used in ack_wait(). response_time is also used in ack_wait and stores the time we sent the message, so we know when the ack message times out
         #####We only enter the statement if we dont have anything in backup_path already.
-        print(len(node.backup_path))
+        #print(len(node.backup_path))
         if len(node.backup_path) == 0:
             node.backup_path = node.path
             timer = datetime.datetime.now().strftime("%d-%m-%y %H:%M:%S")
@@ -343,6 +348,7 @@ async def resp_data():
         data = bytes([int(send_to)>>8]) + bytes([int(send_to)&0xff]) + bytes([offset_frequence]) + str(seperate).encode() + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + str(seperate).encode() + str(ack_id).encode() + str(seperate).encode() + str(path).encode() + str(seperate).encode() + str(temp).encode() + str(seperate).encode() + str(node.backup_path).encode() + str(seperate).encode()
         await asyncio.sleep(0.2)
         node.send(data)
+        print(data)
         print("we send the response, waiting for ack")
         #####Clean the node's path after sending the message
         node.backup_path = ""
@@ -361,7 +367,7 @@ async def ret_data():
         payload = node.data[0]
         path = node.data[1]
         backup_path = node.data[2]
-        print("sender ret data 2")
+        #print("sender ret data 2")
         send_to = int(path[-1])
 
         if len(path) == 1:
@@ -376,6 +382,7 @@ async def ret_data():
         data = bytes([int(send_to)>>8]) + bytes([int(send_to)&0xff]) + bytes([offset_frequence]) + str(seperate).encode() + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + str(seperate).encode() + str(ack_id).encode() + str(seperate).encode() + str(path).encode() + str(seperate).encode() + str(payload).encode() + str(seperate).encode() + str(backup_path).encode() + str(seperate).encode()
         await asyncio.sleep(0.2)
         node.send(data)
+        print(data)
         print("Forwarding return data to next hop")
         #####Clean the node's data after sending the message
         node.data = ("","")
