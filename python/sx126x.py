31dffbfdfa0ddbc21c3e7d941cddee2a1b13dfe6
jenner231 <jenner231@users.noreply.github.com>
2022-06-05 20:51:24 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -464,8 +464,8 @@ class sx126x:
 
             print("receive checkpoint 3")
             #print("We received a message from " +str(int((r_buff[1]<<8) + r_buff[2])) + " with ack id " +str(int(chr(r_buff[5]))))
-            print(type(r_buff[5]))
             print(r_buff)
+            print(type(r_buff[5]))
             print(r_buff[0])
             print(r_buff[1])
             print(r_buff[2])
