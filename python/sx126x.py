cd8e6f343ee2746fa85c5f2858a071e250a4af14
jenner231 <jenner231@users.noreply.github.com>
2022-06-08 09:56:31 +0200

    Update sx126x.py

--- python/sx126x.py
+++ python/sx126x.py
@@ -473,7 +473,7 @@ class sx126x:
             rec = str(r_buff)
             r_buff_in_string = rec.split(",")
             print(r_buff[5])
-            print(sizeof(r_buff[5]))
+            print(len(r_buff[5]))
             #print("receive checkpoint 3")
             #####Made a check to see if the message was for us
             #r_buff[0] == receiving node address, r_buff[1] == sender node address, r_buff[2] == frequency, r_buff[3] == node_id of receiver, r_buff[4] == sender node_id, r_buff[5] == ack_id, r_buff[6]+ == payload
